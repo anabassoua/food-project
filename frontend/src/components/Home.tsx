7ca264fb1601d204aa2079265cc5017f12f8414e
Askia <anabassoua@gmail.com>
2024-02-22 00:03:32 -0500

    fix minor bug

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,6 @@
 // import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 type Recipe = {
   id: number;
@@ -7,13 +8,25 @@ type Recipe = {
 };
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
   const recipes = useLoaderData() as Recipe[];
+
+  const handleSearch = () => {
+    navigate(`/recipes?ingredients=${searchValue}`);
+  };
 
   return (
     <div className="container">
       <h1>What's in your Fridge ?</h1>
-      <input type="text" className="search-bar" placeholder="Search..." />
-      <button>Search</button>
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="e.g: apple,flour,sugar..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
       <div className="trending-recipes">
         <div className="recipe-container">
           {recipes &&
--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -1,3 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+
+type Recipe = {
+  id: number;
+  title: string;
+  image: string;
+};
+
 export default function Recipes() {
-  return <h1>Recipes</h1>;
+  const recipes = useLoaderData() as Recipe[];
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes &&
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image} />
+          </div>
+        ))}
+    </div>
+  );
 }
